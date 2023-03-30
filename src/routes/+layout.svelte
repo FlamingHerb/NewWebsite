5a02f54543bbb214ba37e95d073c7fdea9b3f06f
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-03-30 15:24:12 +0800

    :hammer: Added new pages and fixed stuff and added where user is currently in lemaw.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
   import taglineDatabase from '$lib/taglines.json';
   import { navigating } from '$app/stores';
+  import { page } from '$app/stores';
 
   let logosrc = '/images/fa.png';
 
@@ -18,7 +19,7 @@
     <span id="placeholder">
       VACCARIA
     </span>
-    
+
     <a href="https://github.com/FlamingHerb">
       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></a>
     <a href="https://www.linkedin.com/in/msbbomisol/">
@@ -35,10 +36,10 @@
     
   </div>
   <nav>
-    <a href="/">HOME</a>
-    <a href="/works">WORKS</a>
-    <a href="/blog">BLOG</a>
-    <a href="/about">ABOUT</a>
+    <a class="{$page.route.id === '/' ? 'current' : ''}" href="/">HOME</a>
+    <a class="{$page.route.id === '/works' ? 'current' : ''}" href="/works">WORKS</a>
+    <a class="{$page.route.id === '/blog' ? 'current' : ''}" href="/blog">BLOG</a>
+    <a class="{$page.route.id === '/about' ? 'current' : ''}" href="/about">ABOUT</a>
   </nav>
 </div>
 
@@ -109,7 +110,14 @@
       &:hover{
         color: #ff9600;
       }
+
+      
     }
+  }
+
+  .current{
+    text-decoration: underline;
+    color: #ff9600;
   }
 
   #header {
