ccbd0e3ba3dbf6611eb8422e536d54f80a23dfbb
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-04-19 15:39:56 +0800

    :construction: Adding works page.

--- src/routes/aboutme/+page.svelte
+++ src/routes/aboutme/+page.svelte
@@ -21,14 +21,26 @@
     <title>About Me | Vaccaria's Website</title>
 </head>
 
-<div id="containerbody">
-    <div id="intro">
+<div class="container wrapper column-direction">
+    <div class="section">
         <h1>INTRODUCTION</h1>
         <figure id="authorimg">
             <img src="{authorimg}" alt="A witch profile." on:mouseover={handleMouseOver} on:focus={handleMouseOver} on:blur={handleMouseOut} on:mouseout={handleMouseOut}>
             <figcaption>{caption}</figcaption>
         </figure>
         
+        <p>
+            Hello, I am Michael Sean Brian B. Omisol. Also known as Vaccaria. I am a {year - 2001} old game/software developer and writer, who is currently a {year - 2020}rd year BS Computer Science student in the University of the Philippines — Diliman. I have past experience in making games, writing stuff about games, and working with various game devs... including leading a group in the past.
+        </p>
+        <p>
+            If you've noticed why my site is somewhat archaic looking, that's because I made it that way. It's to honor a past that is already gone, fueled rather by whitespace-dominated websites that takes longer to load because your phone struggles to load up all the JavaScript boilerplates you need. But I digress. There's a lot more to learn, and lot to even shatter about modern designs.
+        </p>
+        <p>
+            If you want to learn more about me, there's the About Me page above. Just click on it. <a href="/aboutme">Or right here</a>.
+        </p>
+    </div>
+    <div class="section">
+        <h1>EXPERIENCE</h1>
         <p>
             Hello, I am Michael Sean Brian B. Omisol. Also known as Vaccaria. I am a {year - 2001} old game/software developer and writer, who is currently a {year - 2020}rd year BS Computer Science student in the University of the Philippines — Diliman. I have past experience in making games, writing stuff about games, and working with various game devs... including leading a group in the past.
         </p>
@@ -71,7 +83,8 @@
     }
 
 
-    #intro {
+    .section {
+        
         margin-left: 1.2em;
         margin-right: 1.2em;
 
---
+++ src/routes/works/+page.svelte
@@ -0,0 +1,63 @@
+
+<head>
+    <title>About Me | Vaccaria's Website</title>
+</head>
+
+<div class="container wrapper column-direction">
+    <h1>Games</h1>
+
+    <div class="section">
+        <img class="work-thumbs" src="images/works_thumbnail/placeholder.png" alt="Game profile thumbnail.">
+        <h1>Project Unity</h1>
+        <p>A murder-mystery set in a cold, dark world. This game is made for the partial fulfillment of the CS 192 class.</p>
+    </div>
+
+    <div class="section">
+        <img class="work-thumbs" src="images/works_thumbnail/placeholder.png" alt="Game profile thumbnail.">
+        <h1>-repent loci- useless apologies of a troublesome witch</h1>
+        <p>She keeps doing weird things, they all claim she's a witch. Why does she keep it up, while apologizing?</p>
+    </div>
+
+    <div class="section">
+        <img class="work-thumbs" src="images/works_thumbnail/placeholder.png" alt="Game profile thumbnail.">
+        <h1>Sa Ilalim ng Gabi</h1>
+        <p>A horror game taking the role of a priestess doing her best to avoid the chaos. This game is made for the partial fulfillment of the SCALE Program 2018-2020 and for the Theme Roulette 2 event.</p>
+    </div>
+
+    <div class="section">
+        <img class="work-thumbs" src="images/works_thumbnail/placeholder.png" alt="Game profile thumbnail.">
+        <h1>The Swallow's Descent</h1>
+        <p>A mystery/whodunnit starring William Byske, an investigator for the Arcane Recon Corps.</p>
+    </div>
+
+</div>
+
+<style lang="scss">
+    @import "../node_modules/rfs/scss";
+
+    h1 {
+        display: block;
+        margin-bottom: 0;
+        @include font-size(1.8rem);
+        font-family: 'Segoe UI', Helvetica, sans-serif;
+        font-weight: normal;
+    }
+
+    p {
+        text-align:justify;
+    }
+
+    .section {
+        width: 100%;
+        h1 {
+            @include font-size(1.2rem);
+        }
+        p {
+            @include font-size(0.75rem);
+        }
+    }
+
+    .work-thumbs {
+        float: left;
+    }
+</style>
