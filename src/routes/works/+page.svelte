a549e29e66bce7f945bbfffdcc516d2129eb6a19
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-04-19 16:12:40 +0800

    :construction: Working on constructions page..

--- src/routes/works/+page.svelte
+++ src/routes/works/+page.svelte
@@ -5,30 +5,45 @@
 
 <div class="container wrapper column-direction">
     <h1>Games</h1>
+    <table>
+        <tr>
+            <td>
+                <a href="/"><img src="images/works_thumbnail/project_unity.png" alt="Game profile thumbnail."></a>
+            </td>
+            <td class="description">
+                <h1><a href="/">Project Unity</a></h1>
+                <p>A murder-mystery VN-like game where a detective is invited to a doomed party in an isolated mansion. This game is made for the partial fulfillment of the CS 192 class.</p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <a href="/"><img src="images/works_thumbnail/repent_loci_valentines.jpg" alt="Game profile thumbnail."></a>
+            </td>
+            <td class="description">
+                <h1><a href="/">-repent loci- useless apologies of a troublesome witch</a></h1>
+                <p>She keeps doing weird things, they all claim she's a witch. Why does she keep it up, while apologizing?</p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <a href="/"><img src="images/works_thumbnail/sing.png" alt="Game profile thumbnail."></a>
+            </td>
+            <td class="description">
+                <h1><a href="/">Sa Ilalim ng Gabi</a></h1>
+                <p>A horror game taking the role of a priestess doing her best to avoid the chaos. This game is made for the partial fulfillment of the SCALE Program 2018-2020 and for the Theme Roulette 2 event.</p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <a href="/"><img src="images/works_thumbnail/swallows_descent.png" alt="Game profile thumbnail."></a>
+            </td>
+            <td class="description">
+                <h1><a href="/">The Swallow's Descent</a></h1>
+                <p>A mystery/whodunnit starring William Byske, an investigator for the Arcane Recon Corps.</p>
+            </td>
+        </tr>
+    </table>
 
-    <div class="section">
-        <img class="work-thumbs" src="images/works_thumbnail/placeholder.png" alt="Game profile thumbnail.">
-        <h1>Project Unity</h1>
-        <p>A murder-mystery set in a cold, dark world. This game is made for the partial fulfillment of the CS 192 class.</p>
-    </div>
-
-    <div class="section">
-        <img class="work-thumbs" src="images/works_thumbnail/placeholder.png" alt="Game profile thumbnail.">
-        <h1>-repent loci- useless apologies of a troublesome witch</h1>
-        <p>She keeps doing weird things, they all claim she's a witch. Why does she keep it up, while apologizing?</p>
-    </div>
-
-    <div class="section">
-        <img class="work-thumbs" src="images/works_thumbnail/placeholder.png" alt="Game profile thumbnail.">
-        <h1>Sa Ilalim ng Gabi</h1>
-        <p>A horror game taking the role of a priestess doing her best to avoid the chaos. This game is made for the partial fulfillment of the SCALE Program 2018-2020 and for the Theme Roulette 2 event.</p>
-    </div>
-
-    <div class="section">
-        <img class="work-thumbs" src="images/works_thumbnail/placeholder.png" alt="Game profile thumbnail.">
-        <h1>The Swallow's Descent</h1>
-        <p>A mystery/whodunnit starring William Byske, an investigator for the Arcane Recon Corps.</p>
-    </div>
 
 </div>
 
@@ -47,17 +62,30 @@
         text-align:justify;
     }
 
-    .section {
-        width: 100%;
+
+
+    table {
+        border-collapse: collapse;
+        max-width: 97.5%;
+        
+        tr:nth-child(even) {background-color: #f2f2f2;}
+
         h1 {
+            margin-top: 0;
             @include font-size(1.2rem);
+
+            a {
+                text-decoration: none;
+            }
         }
         p {
             @include font-size(0.75rem);
         }
+
+        .description{
+            vertical-align: top;
+            padding-left: 0.5em;
+        }
     }
 
-    .work-thumbs {
-        float: left;
-    }
 </style>
